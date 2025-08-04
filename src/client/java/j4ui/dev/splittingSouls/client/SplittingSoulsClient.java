b4d97531531e2936ec4bcbd3d95522dccc527b18
Underio3298 <underio3298@gmail.com>
2025-08-04 10:22:58 -0400

    initial commit

---
+++ src/client/java/j4ui/dev/splittingSouls/client/SplittingSoulsClient.java
@@ -0,0 +1,10 @@
+package j4ui.dev.splittingSouls.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SplittingSoulsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/j4ui/dev/splittingSouls/SplittingSouls.java
@@ -0,0 +1,10 @@
+package j4ui.dev.splittingSouls;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SplittingSouls implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
