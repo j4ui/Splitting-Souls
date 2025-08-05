e94a3a0248f596d0e30ac9e06d85f92a84917cea
Lucman00 <129414800+Lucman00@users.noreply.github.com>
2025-08-05 16:35:06 +0200

    Creative inventory

---
+++ src/main/java/j4ui/dev/splittingSouls/SplittingSoulsItemGroups.java
@@ -0,0 +1,23 @@
+package j4ui.dev.splittingSouls;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public final class SplittingSoulsItemGroups {
+    public static final ItemGroup SPLITTING_SOULS = FabricItemGroup.builder()
+            .icon(() -> new ItemStack(ModItems.PURITY_SHARD))
+            .displayName(Text.translatable("itemGroup.splittingsouls.splittingsouls_group"))
+            .entries((context, entries) -> {
+                entries.add(ModItems.PURITY_SHARD);
+            })
+            .build();
+
+    public static void initialize() {
+        Registry.register(Registries.ITEM_GROUP, Identifier.of("splittingsouls", "splittingsouls_group"), ITEM_GROUP);
+    }
+}
