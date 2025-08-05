e6d8e13f3430a842c93127c16e76ea6beb0b7b38
Lucman00 <129414800+Lucman00@users.noreply.github.com>
2025-08-05 17:45:09 +0200

    added basic purity shard to ancient city loottable

---
+++ src/main/java/j4ui/dev/splittingSouls/ModLootTableModifiers.java
@@ -0,0 +1,31 @@
+package j4ui.dev.splittingSouls;
+
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.util.Identifier;
+
+
+public class ModLootTableModifiers {
+    private static final Identifier ANCIENT_CITY_ID
+            = Identifier.of("minecraft", "chests/ancient_city");
+
+    public static void modifyLootTables(){
+        LootTableEvents.MODIFY.register((key, tableBuilder, source, registry) ->{
+            if (ANCIENT_CITY_ID.equals(key.getValue())) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(1f))
+                        .with(ItemEntry.builder(ModItems.PURITY_SHARD))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 2.0f)).build());
+
+                tableBuilder.pool(poolBuilder.build());
+            }
+        });
+
+    }
+}
